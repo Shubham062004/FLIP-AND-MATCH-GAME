a5fa4a9fd4f59117e03bc9085c0d0259d578831b
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-01-07 03:22:03 +0530

    mode

--- mode.js
+++ mode.js
@@ -1,26 +1,26 @@
 // CODER THEME PAGE
-const coderPlay = document.getElementById('coderPlay');
+const coderPlay = document.getElementById('coder');
 
 coderPlay.onclick = () =>{
     location.href = "./coderGame.html";
 }
 
 // ZOMBIE THEME PAGE
-const zombiePlay = document.getElementById('zombiePlay');
+const zombiePlay = document.getElementById('zombie');
 
 zombiePlay.onclick = () =>{
     location.href = "./zombieGame.html";
 }
 
 // GAME THEME PAGE
-const gamePlay = document.getElementById('gamePlay');
+const gamePlay = document.getElementById('game');
 
 gamePlay.onclick = () =>{
     location.href = "./gameGame.html";
 }
 
 // NATURE THEME PAGE
-const naturePlay = document.getElementById('naturePlay');
+const naturePlay = document.getElementById('nature');
 
 naturePlay.onclick = () =>{
     location.href = "./natureGame.html";
